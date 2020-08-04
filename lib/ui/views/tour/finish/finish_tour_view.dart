663be9fbb5034f74fd37e286e14d3549ded13ac4
Matias de Andrea <deandreamatias@gmail.com>
2020-08-04 12:08:41 +0200

    feat: Update finish tour UI

--- lib/ui/views/tour/finish/finish_tour_view.dart
+++ lib/ui/views/tour/finish/finish_tour_view.dart
@@ -1,8 +1,9 @@
 import 'package:flutter/material.dart';
-import 'package:museum_tour/app/constants.dart';
-import 'package:museum_tour/ui/widgets/top_app_bar.dart';
 import 'package:stacked/stacked.dart';
 
+import '../../../../app/constants.dart';
+import '../../../widgets/button_grid.dart';
+import '../../../widgets/top_app_bar.dart';
 import 'finish_tour_viewmodel.dart';
 
 class FinishTourView extends StatelessWidget {
@@ -11,59 +12,124 @@ class FinishTourView extends StatelessWidget {
   Widget build(BuildContext context) {
     return ViewModelBuilder.nonReactive(
       builder: (context, model, child) => Scaffold(
-        body: Container(
-          color: CustomColor.BACKGROUND,
-          child: Column(
-            mainAxisSize: MainAxisSize.max,
-            children: <Widget>[
-              Expanded(
-                flex: 1,
-                child: Stack(
-                  alignment: Alignment.bottomRight,
-                  children: <Widget>[
-                    Container(
-                      margin: const EdgeInsets.only(bottom: 40.0),
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.only(
-                          bottomRight: Radius.circular(32.0),
-                          bottomLeft: Radius.circular(32.0),
-                        ),
-                        color: Colors.white,
-                      ),
+        backgroundColor: CustomColor.BACKGROUND,
+        body: Column(
+          mainAxisSize: MainAxisSize.max,
+          children: <Widget>[
+            Stack(
+              alignment: Alignment.bottomRight,
+              children: <Widget>[
+                Container(
+                  margin: const EdgeInsets.only(bottom: 40.0),
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.only(
+                      bottomRight: Radius.circular(32.0),
+                      bottomLeft: Radius.circular(32.0),
                     ),
-                    Align(
-                      alignment: Alignment.topCenter,
-                      child: TopAppBar(
+                    color: Colors.white,
+                  ),
+                  child: Column(
+                    mainAxisSize: MainAxisSize.min,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: <Widget>[
+                      TopAppBar(
                         title: 'Fin de la exposición',
                         onPressed: () => model.navigateToHome(),
+                        hasImageBackground: false,
                       ),
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.all(16.0),
-                      child: FloatingActionButton.extended(
-                        onPressed: () async => await model.navigateToHome(),
-                        icon: Icon(
-                          Icons.share,
-                          color: CustomColor.TEXT_HIGH,
+                      Padding(
+                        padding: const EdgeInsets.all(16.0),
+                        child: Text(
+                          'Ana Júlia, has finalizado tu visita a la exposición pre colombina',
+                          textAlign: TextAlign.center,
                         ),
-                        backgroundColor: CustomColor.ACCENT,
-                        label: Text(
-                          'COMPARTIR',
-                          style: TextStyle(
-                            color: CustomColor.TEXT_HIGH,
+                      ),
+                      Container(
+                        height: MediaQuery.of(context).size.width * 2 / 3,
+                        width: MediaQuery.of(context).size.width * 2 / 3,
+                        padding: const EdgeInsets.all(8.0),
+                        margin: const EdgeInsets.all(8.0),
+                        child: Center(
+                          child: Text(
+                            'Chalchiuhtlicue',
+                            style: Theme.of(context)
+                                .textTheme
+                                .headline5
+                                .copyWith(color: Colors.white),
+                          ),
+                        ),
+                        decoration: BoxDecoration(
+                          image: const DecorationImage(
+                            image: AssetImage(Assets.SCULPTURE),
+                            fit: BoxFit.cover,
+                          ),
+                          borderRadius: BorderRadius.circular(32.0),
+                          color: Colors.white,
+                          border: Border.all(
+                            width: 1.0,
+                            color: CustomColor.ACCENT,
                           ),
                         ),
                       ),
-                    )
-                  ],
+                      Padding(
+                        padding: const EdgeInsets.all(16.0),
+                        child: Text(
+                          'Parece que esta pieza ha sido la que más te ha gustado. Si quieres puedes compartirla en tus redes sociales',
+                          textAlign: TextAlign.center,
+                        ),
+                      ),
+                      SizedBox(height: 40.0)
+                    ],
+                  ),
                 ),
+                Padding(
+                  padding: const EdgeInsets.all(16.0),
+                  child: FloatingActionButton.extended(
+                    onPressed: () async => await model.navigateToHome(),
+                    icon: Icon(
+                      Icons.share,
+                      color: CustomColor.TEXT_HIGH,
+                    ),
+                    backgroundColor: CustomColor.ACCENT,
+                    label: Text(
+                      'COMPARTIR',
+                      style: TextStyle(
+                        color: CustomColor.TEXT_HIGH,
+                      ),
+                    ),
+                  ),
+                )
+              ],
+            ),
+            Flexible(
+              child: Column(
+                children: <Widget>[
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 16.0),
+                    child: Row(
+                      children: <Widget>[
+                        ButtonGrid(
+                          title: 'Otros museos',
+                        ),
+                        SizedBox(width: 8.0),
+                        Expanded(
+                          child: Text(
+                            'También puedes ver donde se encuentran otros museos de historia de América',
+                            textAlign: TextAlign.center,
+                          ),
+                        )
+                      ],
+                    ),
+                  ),
+                  FlatButton.icon(
+                    onPressed: () async => await model.navigateToHome(),
+                    icon: Icon(Icons.home),
+                    label: Text('INICIO'),
+                  )
+                ],
               ),
-              Expanded(
-                flex: 1,
-                child: Column(),
-              ),
-            ],
-          ),
+            ),
+          ],
         ),
       ),
       viewModelBuilder: () => FinishTourViewModel(),
