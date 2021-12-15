d658fd4d4deef8e6a4358585c1f208004da55738
Matias de Andrea <deandreamatias@gmail.com>
2021-12-15 20:14:27 +0100

    feat: Config i10n

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
 
 import 'app/locator.dart';
 import 'app/router.dart';
 import 'app/theme.dart';
+import 'generated/l10n.dart';
 
 final appRouter = AppRouter();
 
@@ -17,6 +19,13 @@ class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp.router(
+      localizationsDelegates: const [
+        S.delegate,
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+        GlobalCupertinoLocalizations.delegate,
+      ],
+      supportedLocales: S.delegate.supportedLocales,
       title: 'Museum tour',
       darkTheme: CustomTheme().availableThemes[1],
       theme: CustomTheme().availableThemes[0],
